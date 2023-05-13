bbc0e1eb4171c3e19c458157e9a10e78259bf505
Brevillo <oliver.beebe@digipen.edu>
2023-05-12 19:06:04 -0700

    stuff

---
+++ Assets/Scripts/CamerMover.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerMover : MonoBehaviour
+{
+    // class variables
+    [Tooltip("The speed of the camera movement")]
+    public float Speed = 10;
+    public GameObject Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+        
+
+        transform.position += direction * Speed * Time.deltaTime;
+        */
+
+        if(Target != null)
+        {
+            Vector3 newPos = Target.transform.position;
+            newPos.z = transform.position.z;
+            transform.position = newPos;
+        }
+        
+    }
+}
